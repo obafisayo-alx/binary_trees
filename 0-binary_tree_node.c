99f2c05e05a9afc0424b75a62cd2ad3da5e8f81e
obafisayo-alx <sekaniisjoy@gmail.com>
2024-02-28 06:28:44 +0100

    betty style checked for task 0

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -4,7 +4,7 @@
  * binary_tree_node - creates a binary tree node
  * @parent: pointer to the parent node of the node to create
  * @value: value to put in the new node
- * 
+ *
  * Return: pointer to the newly created node
 */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
